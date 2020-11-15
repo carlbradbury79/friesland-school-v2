705e75aac0cb30c30a60de60e12a3e4fd51a0fd1
Carl Bradbury <carl@gnarlydog.co.uk>
2020-11-15 23:34:55 +0000

    Fixed pagination

--- src/components/archive/Pagination.js
+++ src/components/archive/Pagination.js
@@ -29,7 +29,7 @@ const Pagination = ({ catSlug, page, totalPages }) => (
     <PaginationWrapper>
       {page > 1 ? (
         <Link
-          to={`/blog/${catSlug}/${page === 2 ? "" : page - 1}/`}
+          to={`/${catSlug}/${page === 2 ? "" : page - 1}/`}
           className="navBack"
         >
           <FontAwesomeIcon icon={faCaretLeft} />
@@ -38,15 +38,15 @@ const Pagination = ({ catSlug, page, totalPages }) => (
       ) : null}
 
       {page - 2 > 0 && page - 2 !== 1 ? (
-        <Link to={`/blog/${catSlug}/${page - 2}/`}>{page - 2}</Link>
+        <Link to={`/${catSlug}/${page - 2}/`}>{page - 2}</Link>
       ) : page - 2 === 1 ? (
-        <Link to={`/blog/${catSlug}/`}>{page - 2}</Link>
+        <Link to={`/${catSlug}/`}>{page - 2}</Link>
       ) : null}
 
       {page - 1 > 0 && page - 1 !== 1 ? (
-        <Link to={`/blog/${catSlug}/${page - 1}/`}>{page - 1}</Link>
+        <Link to={`/${catSlug}/${page - 1}/`}>{page - 1}</Link>
       ) : page - 1 === 1 ? (
-        <Link to={`/blog/${catSlug}/`}>{page - 1}</Link>
+        <Link to={`/${catSlug}/`}>{page - 1}</Link>
       ) : null}
 
       {/* Middle Page of 5 */}
@@ -57,7 +57,7 @@ const Pagination = ({ catSlug, page, totalPages }) => (
             color: "var(--primary)",
             border: "2px solid var(--primary)",
           }}
-          to={`/blog/${catSlug}/`}
+          to={`/${catSlug}/`}
         >
           {page}
         </Link>
@@ -68,21 +68,21 @@ const Pagination = ({ catSlug, page, totalPages }) => (
             color: "var(--primary)",
             border: "2px solid var(--primary)",
           }}
-          to={`/blog/${catSlug}/${page}/`}
+          to={`/${catSlug}/${page}/`}
         >
           {page}
         </Link>
       )}
 
       {page + 1 <= totalPages && (
-        <Link to={`/blog/${catSlug}/${page + 1}/`}>{page + 1}</Link>
+        <Link to={`/${catSlug}/${page + 1}/`}>{page + 1}</Link>
       )}
       {page + 2 <= totalPages && (
-        <Link to={`/blog/${catSlug}/${page + 2}/`}>{page + 2}</Link>
+        <Link to={`/${catSlug}/${page + 2}/`}>{page + 2}</Link>
       )}
 
       {page < totalPages ? (
-        <Link to={`/blog/${catSlug}/${page + 1}/`}>
+        <Link to={`/${catSlug}/${page + 1}/`}>
           {/* <span>Next </span> */}
           <FontAwesomeIcon icon={faCaretRight} />
         </Link>
